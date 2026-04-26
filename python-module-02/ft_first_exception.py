95b6a350d22f5ab3a929c9bdb3f692805ce4a6ef
Isabella de Souza Nascimento <isabellanascimento@MacBook-Air-de-Isabella.local>
2026-04-26 12:55:34 -0300

    ex00

---
+++ python-module-02/ft_first_exception.py
@@ -0,0 +1,28 @@
+def input_temperature(temp_str):
+    return int(temp_str)
+
+
+def test_temperature():
+    test_values = ["25", "abc"]
+
+    for value in test_values:
+        print(f"\nInput data is '{value}'")
+        try:
+            temp = input_temperature(value)
+            print(f"Temperature is now {temp}°C")
+
+        except ValueError as e:
+            print(
+                f"Caught input_temperature error: invalid literal for int() with base 10: '{e}'"
+            )
+    print()
+
+    print("\nAll tests completed –– program didn't crash!")
+
+
+def ft_first_exception():
+    print("=== Garden Temperature ===")
+    test_temperature()
+
+
+ft_first_exception()
